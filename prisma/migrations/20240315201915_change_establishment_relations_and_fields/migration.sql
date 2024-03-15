d99b9a95f56c8e44c174360a3be437bf57a67739
Esdras de Melo Motta <emelomotta@gmail.com>
2024-03-15 18:38:52 -0300

    refactor: make changes on tables

---
+++ prisma/migrations/20240315201915_change_establishment_relations_and_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE `addresses` MODIFY `user_id` VARCHAR(191) NULL,
+    MODIFY `establishment_id` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `establishments` MODIFY `status` BOOLEAN NULL DEFAULT true;
