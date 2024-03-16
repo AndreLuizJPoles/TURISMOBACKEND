55b31b7a3b640c048c479a37eac36c21f0a72bae
Esdras de Melo Motta <emelomotta@gmail.com>
2024-03-15 23:14:31 -0300

    refactor: change fields validation

--- src/adapters/utils/apiFieldsValidation/establishmentFieldsValidation.adapter.ts
+++ src/adapters/utils/apiFieldsValidation/establishmentFieldsValidation.adapter.ts
@@ -9,8 +9,6 @@ export class EstablishmentAPIFieldsValidationAdapter
   implements IEstablishmentFieldsValidationPort
 {
   create(data: ICreateEstablishmentUseCaseDataIn): void | Error {
-    const { address, workingTime, ...establishment } = data;
-
     const establishmentSchema = zod.object({
       name: zod.string(),
       email: zod.string().email({
@@ -28,33 +26,40 @@ export class EstablishmentAPIFieldsValidationAdapter
       category_id: zod.string().uuid({
         message: "A categoria informada não é valida.",
       }),
+      address: zod.object(
+        {
+          city: zod.string(),
+          street: zod.string(),
+          number: zod.number().int({
+            message: "O número do endereço deve ser inteiro.",
+          }),
+          neighborhood: zod.string(),
+          complement: zod.string().optional(),
+          zip_code: zod.string(),
+        },
+        {
+          required_error: "Os dados de endereço não foram informados.",
+        }
+      ),
+      workingTime: zod.object(
+        {
+          open_on_sunday: zod.boolean(),
+          open_on_monday: zod.boolean(),
+          open_on_tuesday: zod.boolean(),
+          open_on_wednesday: zod.boolean(),
+          open_on_thursday: zod.boolean(),
+          open_on_friday: zod.boolean(),
+          opening_time: zod.string(),
+          closing_time: zod.string()
+        },
+        {
+          required_error:
+            "Os dados de horário de funcionamento não foram informados.",
+        }
+      ),
     });
 
-    establishmentSchema.parse(establishment);
-
-    const addressSchema = zod.object({
-      city: zod.string(),
-      street: zod.string(),
-      number: zod.number().int({
-        message: "O número do endereço deve ser inteiro.",
-      }),
-      neighborhood: zod.string(),
-      complement: zod.string().optional(),
-      zip_code: zod.string(),
-    });
-
-    addressSchema.parse(address);
-
-    const workingTimeSchema = zod.object({
-      open_on_sunday: zod.boolean(),
-      open_on_monday: zod.boolean(),
-      open_on_tuesday: zod.boolean(),
-      open_on_wednesday: zod.boolean(),
-      open_on_thursday: zod.boolean(),
-      open_on_friday: zod.boolean(),
-    });
-
-    workingTimeSchema.parse(workingTime);
+    establishmentSchema.parse(data);
   }
 
   update(data: IUpdateEstablishmentUseCaseDataIn): void | Error {
@@ -62,12 +67,6 @@ export class EstablishmentAPIFieldsValidationAdapter
 
     const establishmentSchema = zod.object({
       name: zod.string(),
-      email: zod.string().email({
-        message: "Informe um email válido",
-      }),
-      password: zod.string().min(8, {
-        message: "Sua senha deve ter pelo menos 8 caracteres.",
-      }),
       cnpj: zod.string().length(14, {
         message: "O CNPJ informado deve conter 14 caracteres.",
       }),
@@ -77,40 +76,33 @@ export class EstablishmentAPIFieldsValidationAdapter
       category_id: zod.string().uuid({
         message: "A categoria informada não é valida.",
       }),
+      address: zod
+        .object({
+          city: zod.string(),
+          street: zod.string(),
+          number: zod.number().int({
+            message: "O número do endereço deve ser inteiro.",
+          }),
+          neighborhood: zod.string(),
+          complement: zod.string().optional(),
+          zip_code: zod.string(),
+        })
+        .optional(),
+      workingTime: zod
+        .object({
+          open_on_sunday: zod.boolean().optional(),
+          open_on_monday: zod.boolean().optional(),
+          open_on_tuesday: zod.boolean().optional(),
+          open_on_wednesday: zod.boolean().optional(),
+          open_on_thursday: zod.boolean().optional(),
+          open_on_friday: zod.boolean().optional(),
+          opening_time: zod.string().optional(),
+          closing_time: zod.string().optional()
+        })
+        .optional(),
     });
 
     establishmentSchema.parse(establishment);
-
-    if (address) {
-      const addressSchema = zod.object({
-        city: zod.string().optional(),
-        street: zod.string().optional(),
-        number: zod
-          .number()
-          .int({
-            message: "O número do endereço deve ser inteiro.",
-          })
-          .optional(),
-        neighborhood: zod.string().optional(),
-        complement: zod.string().optional(),
-        zip_code: zod.string().optional(),
-      });
-
-      addressSchema.parse(address);
-    }
-
-    if (workingTime) {
-      const workingTimeSchema = zod.object({
-        open_on_sunday: zod.boolean().optional(),
-        open_on_monday: zod.boolean().optional(),
-        open_on_tuesday: zod.boolean().optional(),
-        open_on_wednesday: zod.boolean().optional(),
-        open_on_thursday: zod.boolean().optional(),
-        open_on_friday: zod.boolean().optional(),
-      });
-
-      workingTimeSchema.parse(workingTime);
-    }
   }
 
   getById(id: string): void | Error {
